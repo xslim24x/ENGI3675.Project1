e0f78e6f550b950555a38d081904e62d5ba6b9d0
Shahood Mirza <ijaz_mirza@hotmail.com>
2015-01-17 04:31:13 +0000

    git-tfs-id: [https://levimmartins.visualstudio.com]$/First Assignment;C12

---
+++ WebApplication3/WebApplication3/PaintTable.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PaintTable.aspx.cs" Inherits="WebApplication3.PaintTable" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Table ID="tblPaints" runat="server" BorderWidth="1">
+            <asp:TableHeaderRow>
+                <asp:TableHeaderCell ColumnSpan="2">
+                    Paints
+                </asp:TableHeaderCell>
+            </asp:TableHeaderRow>
+            <asp:TableRow>
+                <asp:TableCell BackColor="#94D4E9">
+                    Paint Name
+                </asp:TableCell>
+                <asp:TableCell BackColor="#94D4E9">
+                    Litres
+                </asp:TableCell>
+            </asp:TableRow>   
+        </asp:Table> 
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebApplication3/PaintTable.aspx.cs
@@ -0,0 +1,51 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class PaintTable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1; Port=5432; Database=Assignment1; User Id=postgres; Password=3675;");
+            conn.Open();
+
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM public.\"Paints\"", conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                string column1 = String.Format("{0}\n", dr[0]);
+                string column2 = String.Format("{0}\n", dr[1]);
+
+                TableRow NewRow1 = new TableRow();
+                TableCell NewCell1 = new TableCell();
+                TableCell NewCell2 = new TableCell();
+
+                NewRow1.Cells.Add(NewCell1);
+                NewRow1.Cells.Add(NewCell2);
+
+                Label newLable1 = new Label();
+                Label newLable2 = new Label();
+
+                newLable1.Text = column1;
+                newLable2.Text = column2;
+
+                NewCell1.Controls.Add(newLable1);
+                NewCell2.Controls.Add(newLable2);
+
+                NewRow1.Cells.Add(NewCell1);
+                NewRow1.Cells.Add(NewCell2);
+                
+                tblPaints.Rows.Add(NewRow1);
+            }
+
+            conn.Close();
+        }
+    }
+}
