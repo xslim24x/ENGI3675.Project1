a5ea1e17ac12e3ba037edfdac0b2b56f4915fe1a
Leví Monteiro Martins <levimmartins@hotmail.com>
2015-01-29 01:14:43 +0000

    git-tfs-id: [https://levimmartins.visualstudio.com]$/First Assignment;C28

--- Project1/Data Base/ddl.sql
+++ Project1/Data Base/ddl.sql
@@ -1,1 +1,18 @@
 ﻿
+-- Table: to store rows about ...;
+
+
+
+
+
+CREATE TABLE "Project1"."Project 1"
+(
+  "Paint Names" text NOT NULL,
+  "Litres" integer,
+  CONSTRAINT "Paint Names" PRIMARY KEY ("Paint Names")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Project1"."Project 1"
+  OWNER TO postgres;
